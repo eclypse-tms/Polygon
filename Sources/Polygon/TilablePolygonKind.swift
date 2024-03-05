307078e6f4d51cc0106060347f7a340c654c5519
Deniz Nessa <deniz@eclypse.io>
2024-03-05 00:18:20 -0600

    getting close

--- Sources/Polygon/TilablePolygonKind.swift
+++ Sources/Polygon/TilablePolygonKind.swift
@@ -5,18 +5,26 @@
 //  Created by eclypse on 3/4/24.
 //
 
-import Foundation
+import SwiftUI
 
 public enum TilablePolygonKind: Int {
     case equilateralTriangle
     case square
     case hexagon
     
-    var numberOfSides: Int {
+    public var numberOfSides: Int {
         switch self {
         case .equilateralTriangle: return 3
         case .square: return 4
         case .hexagon: return 6
+        }
+    }
+    
+    public var defaultRotation: Angle {
+        switch self {
+        case .equilateralTriangle: return Angle(degrees: 30)
+        case .square: return Angle(degrees: 45)
+        case .hexagon: return Angle(degrees: 0)
         }
     }
 }
--- Sources/Polygon/TiledPolygon.swift
+++ Sources/Polygon/TiledPolygon.swift
@@ -10,95 +10,65 @@ import SwiftUI
 /// Polygon allows you to add a polygon of any sides to your SwiftUI based views
 public struct TiledPolygon: View {
     
-    public internal (set) var _kind: TilablePolygonKind
+    @usableFromInline 
+    internal var _kind: TilablePolygonKind
     /// kind of polygon that can be tiled on a euclydian plane
     @inlinable public func kind(_ kind: TilablePolygonKind) -> TiledPolygon {
-        return TiledPolygon(kind: kind,
-                            fillColor: self._fillColor,
-                            rotationAngle: self._rotationAngle,
-                            borderWidth: self._borderWidth,
-                            borderColor: self._borderColor,
-                            fixedTileSize: self._fixedTileSize,
-                            flexibleTiling: self._flexibleTiling)
+        var newCopy = self
+        newCopy._kind = kind
+        newCopy._rotationAngle = kind.defaultRotation
+        return newCopy
     }
     
     
-    public internal (set) var _fillColor: Color = .blue
+    @usableFromInline
+    internal var _fillColor: Color = .blue
     /// the color to fill the polygon with
     @inlinable public func fillColor(_ color: Color) -> TiledPolygon {
-        return TiledPolygon(kind: self._kind,
-                            fillColor: color,
-                            rotationAngle: self._rotationAngle,
-                            borderWidth: self._borderWidth,
-                            borderColor: self._borderColor,
-                            fixedTileSize: self._fixedTileSize,
-                            flexibleTiling: self._flexibleTiling)
+        var newCopy = self
+        newCopy._fillColor = color
+        return newCopy
     }
     
-    public internal (set) var _rotationAngle: Angle = .zero
-    
+    @usableFromInline
+    internal var _rotationAngle: Angle = .zero
     /// Rotation angle expressed in degrees (45°) or radians (π/4)
     @inlinable public func rotationAngle(_ angle: Angle) -> TiledPolygon {
-        return TiledPolygon(kind: self._kind,
-                            fillColor: self._fillColor,
-                            rotationAngle: angle,
-                            borderWidth: self._borderWidth,
-                            borderColor: self._borderColor,
-                            fixedTileSize: self._fixedTileSize,
-                            flexibleTiling: self._flexibleTiling)
+        var newCopy = self
+        newCopy._rotationAngle = angle
+        return newCopy
     }
     
     
-    public internal (set) var _borderWidth: CGFloat = 1.0
-    
+    @usableFromInline 
+    internal var _interTileSpacing: CGFloat = 1.0
     /// optional border width. specify zero to negate the border
-    @inlinable public func borderWidth(_ width: CGFloat) -> TiledPolygon {
-        return TiledPolygon(kind: self._kind,
-                            fillColor: self._fillColor,
-                            rotationAngle: self._rotationAngle,
-                            borderWidth: width,
-                            borderColor: self._borderColor,
-                            fixedTileSize: self._fixedTileSize,
-                            flexibleTiling: self._flexibleTiling)
+    @inlinable public func interTileSpacing(_ spacing: CGFloat) -> TiledPolygon {
+        var newCopy = self
+        newCopy._interTileSpacing = spacing
+        return newCopy
     }
     
-    public internal (set) var _borderColor: Color = .black
-    
-    /// optional border color. only works when the border width is greater than 0
-    @inlinable public func borderColor(_ color: Color) -> TiledPolygon {
-        return TiledPolygon(kind: self._kind,
-                            fillColor: self._fillColor,
-                            rotationAngle: self._rotationAngle,
-                            borderWidth: self._borderWidth,
-                            borderColor: color,
-                            fixedTileSize: self._fixedTileSize,
-                            flexibleTiling: self._flexibleTiling)
-    }
-    
-    public internal (set) var _fixedTileSize = CGSize(width: 64, height: 64)
-    
+    @usableFromInline 
+    internal var _fixedTileSize = CGSize(width: 64, height: 64)
     /// setting the fixed tile size remove the previous Tiling option
     @inlinable public func fixedTileSize(_ size: CGSize) -> TiledPolygon {
-        return TiledPolygon(kind: self._kind,
-                            fillColor: self._fillColor,
-                            rotationAngle: self._rotationAngle,
-                            borderWidth: self._borderWidth,
-                            borderColor: self._borderColor,
-                            fixedTileSize: size,
-                            flexibleTiling: self._flexibleTiling)
+        var newCopy = self
+        newCopy._fixedTileSize = size
+        newCopy._flexibleTiling = nil
+        return newCopy
     }
     
-    public internal (set) var _flexibleTiling: FlexibleTiling?
-    
-    /// setting the flexible tiling parameters override the fixed tile size
-    @inlinable public func flexibleTiling(_ flexibleTiling: FlexibleTiling) -> TiledPolygon {
-        return TiledPolygon(kind: self._kind,
-                            fillColor: self._fillColor,
-                            rotationAngle: self._rotationAngle,
-                            borderWidth: self._borderWidth,
-                            borderColor: self._borderColor,
-                            fixedTileSize: self._fixedTileSize,
-                            flexibleTiling: flexibleTiling)
+    @usableFromInline 
+    internal var _flexibleTiling: CGFloat?
+    /// indicates the number of tiles that should be placed horizontally.
+    /// this number also dictates how many tiles can be placed vertically
+    /// depending on how many you can fit horizontally.
+    @inlinable 
+    public func flexibleTiling(_ flexibleTiling: CGFloat) -> TiledPolygon {
+        var newCopy = self
+        newCopy._flexibleTiling = flexibleTiling
+        return newCopy
     }
     
     private var _originOffset = CGPoint(x: 0, y: 0)
@@ -106,12 +76,10 @@ public struct TiledPolygon: View {
     public init(kind: TilablePolygonKind = .square,
                 fillColor: Color = .blue,
                 rotationAngle: Angle = .zero,
-                borderWidth: CGFloat = 1.0,
-                borderColor: Color = .black,
+                interTileSpacing: CGFloat = 1.0,
                 fixedTileSize: CGSize = .init(width: 64, height: 64),
-                flexibleTiling: FlexibleTiling? = nil) {
-        self._borderWidth = borderWidth
-        self._borderColor = borderColor
+                flexibleTiling: CGFloat? = nil) {
+        self._interTileSpacing = interTileSpacing
         self._fillColor = fillColor
         self._kind = kind
         self._rotationAngle = rotationAngle
@@ -127,10 +95,21 @@ public struct TiledPolygon: View {
             let numberOfVerticalTiles: Int
             let effectiveTileSize: CGSize
             if let validFlexibleTiling = _flexibleTiling {
-                numberOfHorizontalTiles = Int(validFlexibleTiling.numberOfHorizontalTiles.rounded(.up))
-                let canvasSizeAfterInterTilingGapIsApplied = canvasSize.width - (CGFloat(numberOfHorizontalTiles - 1) * validFlexibleTiling.interTilingSpace)
-                let widthOfEachTile = canvasSizeAfterInterTilingGapIsApplied / CGFloat(validFlexibleTiling.numberOfHorizontalTiles)
-                let heightOfEachTile = widthOfEachTile
+                numberOfHorizontalTiles = Int(validFlexibleTiling.rounded(.up))
+                let canvasSizeAfterInterTilingGapIsApplied = canvasSize.width - (CGFloat(numberOfHorizontalTiles - 1) * _interTileSpacing)
+                let widthOfEachTile = canvasSizeAfterInterTilingGapIsApplied / validFlexibleTiling
+                let heightOfEachTile: CGFloat
+                
+                switch _kind {
+                case .equilateralTriangle:
+                    let angle = Angle(degrees: (90 - _rotationAngle.degrees)).radians
+                    heightOfEachTile = widthOfEachTile * sin(angle)
+                case .square:
+                    heightOfEachTile = widthOfEachTile
+                case .hexagon:
+                    heightOfEachTile = widthOfEachTile * sin(Angle(degrees: 60).radians)
+                }
+                 
                 numberOfVerticalTiles = Int((canvasSize.height / heightOfEachTile).rounded(.up))
                 effectiveTileSize = CGSize(width: widthOfEachTile,height: heightOfEachTile)
             } else {
@@ -147,15 +126,8 @@ public struct TiledPolygon: View {
                 for eachColumn in 0..<numberOfVerticalTiles {
                     //when eachColumn is 0, we are tiling the first column on the top
                     
-                    let tileXOffset: CGFloat
-                    let tileYOffset: CGFloat
-                    if let validFlexibleTiling = _flexibleTiling {
-                        tileXOffset = CGFloat(eachLine) * (effectiveTileSize.width + validFlexibleTiling.interTilingSpace)
-                        tileYOffset = CGFloat(eachColumn) * (effectiveTileSize.height + validFlexibleTiling.interTilingSpace)
-                    } else {
-                        tileXOffset = CGFloat(eachLine) * effectiveTileSize.width
-                        tileYOffset = CGFloat(eachColumn) * effectiveTileSize.height
-                    }
+                    let tileXOffset =  CGFloat(eachLine) * (effectiveTileSize.width + _interTileSpacing)
+                    let tileYOffset = CGFloat(eachColumn) * (effectiveTileSize.height + _interTileSpacing)
                     
                     let boundingRect = CGRect(origin: CGPoint(x: tileXOffset, y: tileYOffset), size: effectiveTileSize)
                     
@@ -166,13 +138,12 @@ public struct TiledPolygon: View {
                     // the polygon points will be located on a circle - hence the radius calculation
                     // this radius calculation also takes into account the border width which gets
                     // added on the outside of the shape
-                    let radius = min(boundingRect.width, boundingRect.height) / 2.0 - _borderWidth / 2.0
+                    let radius = min(boundingRect.width, boundingRect.height) / 2.0 - _interTileSpacing / 2.0
                     
                     let initialPath = drawInitialPolygonPath(centerPoint: centerPoint, radius: radius)
                     
                     let scaledPolygonPath = scale(originalPath: initialPath, rect: boundingRect, originalCenter: centerPoint, reCenter: true)
       
-                    context.stroke(scaledPolygonPath, with: .color(_borderColor), style: StrokeStyle(lineWidth: _borderWidth))
                     context.fill(scaledPolygonPath, with: .color(_fillColor))
                 }
             }
@@ -206,8 +177,8 @@ public struct TiledPolygon: View {
     private func scale(originalPath: Path, rect: CGRect, originalCenter: CGPoint, reCenter: Bool) -> Path {
         // 1. calculate the scaling factor to touch all the edges
         let boundingRectOfRotatedPath = originalPath.boundingRect
-        let scaleFactorX = rect.width / (boundingRectOfRotatedPath.width + _borderWidth/2.0)
-        let scaleFactorY = rect.height / (boundingRectOfRotatedPath.height + _borderWidth/2.0)
+        let scaleFactorX = rect.width / (boundingRectOfRotatedPath.width)
+        let scaleFactorY = rect.height / (boundingRectOfRotatedPath.height)
         let finalScaleFactor = min(scaleFactorX, scaleFactorY)
         
         if abs(finalScaleFactor - 1.0) < 0.000001 {
@@ -235,14 +206,13 @@ public struct TiledPolygon: View {
 }
 
 #Preview {
+    let backgroundColor = Color(white: 0.85)
     let xxx =
     TiledPolygon()
         .kind(.equilateralTriangle)
-        .rotationAngle(Angle(degrees: 75))
-        .borderWidth(1)
-        .borderColor(.black)
-        .flexibleTiling(FlexibleTiling(numberOfHorizontalTiles: 4))
-        .background(Color.yellow)
+        .interTileSpacing(2)
+        .flexibleTiling(4)
+        .background(backgroundColor)
         .padding()
     return xxx
 }
